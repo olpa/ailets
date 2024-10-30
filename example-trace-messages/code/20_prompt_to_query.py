be4aa7c8cb4b97708f4351140ddb17359f849c9e
Oleg Parashchenko <olpa@uucode.com>
2024-10-30 04:13:00 +0100

    Trying prompt to code with messaging only.

---
+++ example-trace-messages/code/20_prompt_to_query.py
@@ -0,0 +1,37 @@
+import json
+import sys
+
+
+(_, in_file, out_file) = sys.argv
+
+const url = "https://api.openai.com/v1/chat/completions"
+
+
+with open(in_file) as h:
+    prompt = json.load(h)
+
+auth_key = {
+          "actor": "auth-key",
+          "outputStreamName": f"auth-{url}",
+          "inputStream": {
+              "url": url,
+              "outputPrefix": "Bearer ",
+              }
+        }
+
+query = {
+  "$deps": [auth_key],
+  "url": url,
+  "method": "POST",
+  "headers": {
+    "Content-type": "application/json",
+    "Authorization": { "$stream": f"auth-{url}" }
+  },
+  body: {
+    "model": "gpt-3.5-turbo",
+    "messages": prompt
+  }
+}
+
+with open(out_file, "w") as h:
+    json.dump(query, h, indent=2)
