802a6a0053d92a06c23277c2f0096aa439099ff3
Oleg Parashchenko <olpa@uucode.com>
2024-11-08 06:39:43 +0100

    No need for traces anymore.

--- example-trace-messages/10_basic_hello.py
+++
@@ -1,7 +0,0 @@
-import localsetup
-from ailets.cons import mkenv, prompt_to_md, build_plan_writing_trace
-
-
-env = mkenv()
-node = prompt_to_md(env)
-build_plan_writing_trace(env, node.name, "traces/basic_hello")
