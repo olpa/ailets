30136deb6de8a443341aa429e45a4f351f52ccc2
Oleg Parashchenko <olpa@uucode.com>
2024-11-25 05:52:57 +0100

    Toolcall chain.

--- pylib-v1/ailets/tools/get_user_name/call.py
+++ pylib-v1/ailets/tools/get_user_name/call.py
@@ -1,10 +1,12 @@
 import os
 from ailets.cons.typing import INodeRuntime
+from ailets.cons.util import write_all
 
 
 def call(runtime: INodeRuntime) -> None:
     """Call the get_user_name tool."""
     value = os.environ["USER"]
 
-    runtime.open_write(None).write(value.encode("utf-8"))
-    runtime.close_write(None)
+    fd = runtime.open_write(None)
+    write_all(runtime, fd, value.encode("utf-8"))
+    runtime.close(fd)
