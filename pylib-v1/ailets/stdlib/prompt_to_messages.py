b5647582661e87f8191554f30e53fecb47c5b681
Oleg Parashchenko <olpa@uucode.com>
2024-11-27 15:29:49 +0100

    Convert a system prompt to a system message.

--- pylib-v1/ailets/stdlib/prompt_to_messages.py
+++ pylib-v1/ailets/stdlib/prompt_to_messages.py
@@ -1,21 +1,27 @@
+import json
+from typing import Any, Dict
 from ailets.cons.typing import INodeRuntime
-from ailets.cons.util import read_all, write_all
+from ailets.cons.util import iter_streams_objects, write_all
 
 
 def prompt_to_messages(runtime: INodeRuntime) -> None:
+    role_to_content: Dict[str, Any] = {}
+    for content_item in iter_streams_objects(runtime, None):
+        role = content_item.get("role", "user")
+        role_to_content.setdefault(role, []).append(content_item)
+
+    keys = list(role_to_content.keys())
+    keys.sort()
+    messages = map(
+        lambda key: {
+            "role": key,
+            "content": role_to_content[key],
+        },
+        keys,
+    )
+
     fd_out = runtime.open_write(None)
-    write_all(runtime, fd_out, b'{"role":"user","content":[')
-
-    i = 0
-    while i < runtime.n_of_streams(None):
-        if i:
-            write_all(runtime, fd_out, b",")
-        fd_in = runtime.open_read(None, i)
-        i += 1
-        write_all(runtime, fd_out, read_all(runtime, fd_in))
-        runtime.close(fd_in)
-
-    write_all(runtime, fd_out, b"]}")
+    write_all(runtime, fd_out, json.dumps(list(messages)).encode("utf-8"))
     runtime.close(fd_out)
 
     for media in runtime.read_dir("media"):
