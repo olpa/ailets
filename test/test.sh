0bc6ce396c2ab1b7705cf36406543934cd6be48f
Oleg Parashchenko <olpa@uucode.com>
2024-11-28 05:55:38 +0100

    Add image generation to tests.

--- test/test.sh
+++ test/test.sh
@@ -35,3 +35,15 @@ echo $div======== gpt4o ======== Link to an image
 ../command-line-tool/ailets0.py gpt4o --prompt "Describe the image." --prompt "@https://gewova.com/assets/ui-blurred.png"
 
 ##
+
+echo $div======== dalle ======== Generate an image, get as a link
+
+../command-line-tool/ailets0.py --prompt 'linux logo'
+
+
+##
+
+echo $div======== dalle ======== Generate an image, get it back
+
+../command-line-tool/ailets0.py --prompt 'response_format="b64_json"\n---\nlinux logo
+
