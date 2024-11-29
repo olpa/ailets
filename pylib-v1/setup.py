7f0889dfe77f432426a797574d7299633d54f16f
Oleg Parashchenko <olpa@uucode.com>
2024-11-29 07:02:46 +0100

    Deploy v0.2.0 (#54)

--- pylib-v1/setup.py
+++ pylib-v1/setup.py
@@ -11,6 +11,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         'requests',
+        'typing_extensions',
     ],
     author="Oleg Parashchenko",
     author_email="olpa@uucode.com",
