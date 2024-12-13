3716d6c280b5a93cd83697affb746d25358d1a28
Oleg Parashchenko <olpa@uucode.com>
2024-12-13 07:20:14 +0100

    Parse the first token, create a loop.

--- ailets-rs/messages_to_markdown/src/lib.rs
+++ ailets-rs/messages_to_markdown/src/lib.rs
@@ -1,4 +1,4 @@
-//use jiter::{Jiter, NumberInt, Peek};
+use jiter::{Jiter};
 
 #[link(wasm_import_module = "")]
 extern "C" {
@@ -11,6 +11,14 @@ extern "C" {
 }
 
 const BUFFER_SIZE: u32 = 1024;
+
+#[derive(Debug, PartialEq)]
+enum State {
+    TopLevel,
+    InsideMessage,
+    InsideBody,
+    // InsideContentItem,
+}
 
 /// Converts a JSON message format to markdown.
 ///
@@ -26,14 +34,38 @@ pub fn messages_to_markdown() {
 
     let input_fd = unsafe { open_read(b"".as_ptr(), 0) };
     let bytes_read = unsafe { aread(input_fd, buffer.as_mut_ptr(), BUFFER_SIZE) };
-    let json_data = std::str::from_utf8(&buffer[..bytes_read as usize]).unwrap();
-    println!("!!!!!!!!!!!!!!!!!!!!!!!!! json_data: {json_data}");
 
-    //let mut jiter = Jiter::new(json_data.as_bytes());
-    //assert_eq!(jiter.next_object().unwrap(), Some("role"));
-    //assert_eq!(jiter.next_str().unwrap(), "assistant");
-    //assert_eq!(jiter.next_key().unwrap(), Some("content"));
-    //assert_eq!(jiter.next_array().unwrap(), Some(Peek::String));
+    let mut jiter = Jiter::new(&buffer[..bytes_read as usize]);
+    let mut state = State::TopLevel;
+    let mut expect_object = true;
+
+    loop {
+        let next = if expect_object { jiter.next_object() } else { jiter.next_key() };
+        println!("! top level next: {next:?} in state: {state:?}");
+        expect_object = false;
+
+        if next.is_err() {
+            println!("Error: {next:?}");  // FIXME
+            break;
+        }
+        let key = next.unwrap();
+
+        if state == State::TopLevel {
+            state = State::InsideMessage;
+        }
+        if state == State::InsideMessage {
+            if key.is_none() {
+                state = State::TopLevel;
+                continue;
+            }
+            if key.unwrap() == "content" {
+                state = State::InsideBody;
+                expect_object = true;
+                continue;
+            }
+        }
+        panic!("Unexpected state: {state:?}");
+    }
 
     let output_fd = unsafe { open_write(b"".as_ptr()) };
     println!("!!!!!!!!!!!!!!!!!!!!!!!!! output_fd: {output_fd}");
