4723612d9bae25fa97f81825d11476aaa1df4c34
Oleg Parashchenko <olpa@uucode.com>
2024-12-18 05:11:04 +0100

    Placeholder for a wasm actor.

---
+++ pylib-v1/ailets/stdlib/messages_to_markdown_wasm.py
@@ -0,0 +1,9 @@
+from ailets.cons.atyping import INodeRuntime
+from ailets.cons.util import write_all
+
+
+async def messages_to_markdown(runtime: INodeRuntime) -> None:
+    fd = await runtime.open_write(None)
+    content = "from wasm placeholder"
+    await write_all(runtime, fd, content.encode("utf-8"))
+    await runtime.close(fd)
