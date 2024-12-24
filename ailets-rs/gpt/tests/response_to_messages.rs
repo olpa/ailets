6696a0ed0ed81a1bea5e5b48d383f166bc54dcd0
Oleg Parashchenko <olpa@uucode.com>
2024-12-24 04:28:19 +0100

    Add fixture: basic response.

--- ailets-rs/gpt/tests/response_to_messages.rs
+++ ailets-rs/gpt/tests/response_to_messages.rs
@@ -6,19 +6,20 @@ use mocked_node_runtime::{clear_mocks, get_output, set_input};
 #[test]
 fn test_basic_processing() {
     clear_mocks();
-    let input = "Hello GPT!";
-    set_input(&[input]);
+    let fixture_content = std::fs::read_to_string("tests/fixture/basic_reponse.txt")
+        .expect("Failed to read fixture file 'basic_reponse.txt'");
+    set_input(&[&fixture_content]);
 
     process_gpt();
 
-    assert_eq!(get_output(), input);
+    assert_eq!(get_output(), fixture_content);
 }
 
 #[test]
 fn test_basic_streaming() {
     clear_mocks();
     let fixture_content = std::fs::read_to_string("tests/fixture/basic_streaming.txt")
-        .expect("Failed to read fixture file");
+        .expect("Failed to read fixture file 'basic_streaming.txt'");
     set_input(&[&fixture_content]);
 
     process_gpt();
