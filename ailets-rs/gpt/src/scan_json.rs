a087596167b04e4ddadccc03d0ea42131550ea57
Oleg Parashchenko <olpa@uucode.com>
2024-12-26 05:50:43 +0100

    Pass: array.

--- ailets-rs/gpt/src/scan_json.rs
+++ ailets-rs/gpt/src/scan_json.rs
@@ -44,32 +44,75 @@ impl<'a, T> Trigger<'a, T> {
     }
 }
 
+#[derive(Debug)]
+struct Context {
+    current_key: String,
+    is_in_object: bool,
+    is_in_array: bool,
+}
+
 #[allow(clippy::missing_panics_doc)]
 pub fn scan_json<T>(triggers: &[Trigger<T>], rjiter: &mut RJiter, _baton: T) {
     println!("scan_json: triggers={triggers:?}");
-    let mut context: Vec<String> = Vec::new();
-    let mut at_object_begin = false;
+    let mut context: Vec<Context> = Vec::new();
+    let mut is_object_begin = false;
     let mut is_in_object = false;
+    let mut is_in_array = false;
     //let mut peeked = Peek::None;
-    let mut peeked = rjiter.peek().unwrap(); // FIXME
+    let mut current_key: String = "#top".to_string();
+    let mut peeked: Option<Peek> = None;
     loop {
+        peeked = None;
+
         if is_in_object {
+            panic!("scan_json: is_in_object={is_in_object}");
+        }
+
+        if is_in_array {
+            let apickedr = if is_object_begin {
+                rjiter.known_array()
+            } else {
+                rjiter.array_step()
+            };
+            peeked = apickedr.unwrap();
+            if peeked == None {
+                let ctx = context.pop().unwrap();
+                current_key = ctx.current_key;
+                is_in_array = ctx.is_in_array;
+                is_in_object = ctx.is_in_object;
+                continue;                                    // continue
+            }
+        }
+
+        if peeked == None {
             let peekedr = rjiter.peek();
-            println!("in object: peeked={peekedr:?}, exit");
-            break;
-        }
+            println!("main loop: peeked={peekedr:?}, exit");
+            if let Err(jiter::JiterError {
+                error_type: jiter::JiterErrorType::JsonError(jiter::JsonErrorType::EofWhileParsingValue),
+                ..
+            }) = peekedr
+            {
+                // TODO: check that we are on top, not inside object or array
+                break;
+            }
 
-        let peekedr = rjiter.peek();
-        if let Err(jiter::JiterError {
-            error_type: jiter::JiterErrorType::JsonError(jiter::JsonErrorType::EofWhileParsingValue),
-            ..
-        }) = peekedr
-        {
-            // TODO: check that we are on top, not inside object or array
-            break;
-        }
+            peeked = Some(peekedr.unwrap());
+        };
 
-        peeked = peekedr.unwrap();
+        let peeked = peeked.unwrap();
+
+        if peeked == Peek::Array {
+            context.push(Context {
+                current_key: current_key,
+                is_in_object: is_in_object,
+                is_in_array: true,
+            });
+            current_key = "#array".to_string();
+            is_in_array = true;
+            is_in_object = false;
+            is_object_begin = true;
+            continue;
+        }
 
         if peeked == Peek::Null {
             rjiter.known_null().unwrap();
