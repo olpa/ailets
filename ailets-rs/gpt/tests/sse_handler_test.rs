3643d634cd4a041c909c5b62b6ce2c5c6a77014a
Oleg Parashchenko <olpa@uucode.com>
2024-12-31 04:57:27 +0100

    Draft of the basic sse test.

---
+++ ailets-rs/gpt/tests/sse_handler_test.rs
@@ -0,0 +1,33 @@
+use std::cell::RefCell;
+use std::io;
+
+mod mocked_node_runtime;
+use mocked_node_runtime::{clear_mocks, get_output, set_input};
+
+use crate::awriter::AWriter;
+use crate::rjiter::RJiter;
+use crate::sse_handler::{on_begin_delta, on_end_delta, on_delta_role, SSEHandler};
+
+
+#[test]
+fn basic_pass() {
+    // Arrange
+    clear_mocks();
+    let input = r#""assistant" "hello""#;
+    let mut rjiter = RJiter::new(io::Cursor::new(input));
+    let awriter = AWriter::new("");
+    let mut handler = SSEHandler::new(awriter);
+
+    // Act
+    on_begin_delta(&RefCell::new(rjiter), &RefCell::new(handler));
+    on_delta_role(&RefCell::new(rjiter), &RefCell::new(handler));
+    on_delta_content(&RefCell::new(rjiter), &RefCell::new(handler));
+    on_end_delta(&RefCell::new(rjiter), &RefCell::new(handler));
+    handler.end();
+
+    // Assert
+    assert_eq!(
+        get_output(),
+        r#"{"role":"assistant","content":[{"type":"text","text":"hello"}]}"#
+    );
+}
