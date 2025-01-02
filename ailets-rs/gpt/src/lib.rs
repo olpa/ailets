92778d64610436ce93e14b61922ca1bd22130e56
Oleg Parashchenko <olpa@uucode.com>
2025-01-02 09:58:51 +0100

    Mini-refactor naming.

--- ailets-rs/gpt/src/lib.rs
+++ ailets-rs/gpt/src/lib.rs
@@ -14,23 +14,23 @@ use scan_json::{scan_json, ActionResult, Matcher, Trigger, TriggerEnd};
 
 const BUFFER_SIZE: u32 = 1024;
 
-fn on_begin_of_message(_rjiter: &RefCell<RJiter>, writer: &RefCell<AWriter>) -> ActionResult {
+fn on_begin_message(_rjiter: &RefCell<RJiter>, writer: &RefCell<AWriter>) -> ActionResult {
     writer.borrow_mut().begin_message();
     ActionResult::Ok
 }
 
-fn on_end_of_message(writer: &RefCell<AWriter>) {
+fn on_end_message(writer: &RefCell<AWriter>) {
     writer.borrow_mut().end_message();
 }
 
-fn on_message_role(rjiter_cell: &RefCell<RJiter>, writer: &RefCell<AWriter>) -> ActionResult {
+fn on_role(rjiter_cell: &RefCell<RJiter>, writer: &RefCell<AWriter>) -> ActionResult {
     let mut rjiter = rjiter_cell.borrow_mut();
     let role = rjiter.next_str().unwrap();
     writer.borrow_mut().role(role);
     ActionResult::OkValueIsConsumed
 }
 
-fn on_message_content(rjiter_cell: &RefCell<RJiter>, writer_cell: &RefCell<AWriter>) -> ActionResult {
+fn on_content(rjiter_cell: &RefCell<RJiter>, writer_cell: &RefCell<AWriter>) -> ActionResult {
     let mut rjiter = rjiter_cell.borrow_mut();
     let peeked = rjiter.peek();
     assert!(
@@ -59,29 +59,29 @@ pub extern "C" fn process_gpt() {
 
     let rjiter_cell = RefCell::new(RJiter::new(&mut reader, &mut buffer));
 
-    let begin_of_message = Trigger::new(
+    let begin_message = Trigger::new(
         Matcher::new("message".to_string(), None, None, None),
-        Box::new(on_begin_of_message),
+        Box::new(on_begin_message),
     );
-    let end_of_message = TriggerEnd::new(
+    let end_message = TriggerEnd::new(
         Matcher::new("message".to_string(), None, None, None),
-        Box::new(on_end_of_message),
+        Box::new(on_end_message),
     );
-    let message_role = Trigger::new(
+    let role = Trigger::new(
         Matcher::new("role".to_string(), Some("message".to_string()), None, None),
-        Box::new(on_message_role),
+        Box::new(on_role),
     );
-    let message_content = Trigger::new(
+    let content = Trigger::new(
         Matcher::new(
             "content".to_string(),
             Some("message".to_string()),
             None,
             None,
         ),
-        Box::new(on_message_content),
+        Box::new(on_content),
     );
-    let triggers = vec![begin_of_message, message_role, message_content];
-    let triggers_end = vec![end_of_message];
+    let triggers = vec![begin_message, role, content];
+    let triggers_end = vec![end_message];
     let sse_tokens = vec!["data:", "DONE"];
     scan_json(
         &triggers,
