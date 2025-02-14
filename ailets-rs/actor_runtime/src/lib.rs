7eabb3b42a120ce0cf5153c0d85836bdc91dc385
Oleg Parashchenko <olpa@uucode.com>
2025-02-14 05:07:44 +0100

    Added a first test, now need to lint.

--- ailets-rs/actor_runtime/src/lib.rs
+++ ailets-rs/actor_runtime/src/lib.rs
@@ -1,3 +1,4 @@
 mod actor_runtime;
+mod mocked_actor_runtime;
 
 pub use actor_runtime::{aclose, aread, awrite, n_of_streams, open_read, open_write};
---
+++ ailets-rs/actor_runtime/tests/mocked_actor_runtime_tests.rs
@@ -0,0 +1,13 @@
+use crate::mocked_actor_runtime::{clear_mocks, open_read};
+use std::ffi::CString;
+
+#[test]
+fn open_read_returns_negative_one_if_no_file() {
+    clear_mocks();
+    
+    let name = CString::new("test").unwrap();
+    let fd = open_read(name.as_ptr(), 0);
+    
+    assert_eq!(fd, -1);
+}
+
