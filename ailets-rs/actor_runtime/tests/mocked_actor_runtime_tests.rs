0f1887683647b54e49c2a790dda627860750c60c
Oleg Parashchenko <olpa@uucode.com>
2025-02-15 04:15:53 +0100

    Tests for n_of_streams.

--- ailets-rs/actor_runtime/tests/mocked_actor_runtime_tests.rs
+++ ailets-rs/actor_runtime/tests/mocked_actor_runtime_tests.rs
@@ -1,5 +1,30 @@
-use actor_runtime::mocked_actor_runtime::{clear_mocks, open_read};
+use actor_runtime::mocked_actor_runtime::{add_file, clear_mocks, n_of_streams, open_read};
 use std::ffi::CString;
+
+#[test]
+fn n_of_streams_returns_zero() {
+    clear_mocks();
+
+    let name = CString::new("test").unwrap();
+    let n = n_of_streams(name.as_ptr());
+
+    assert_eq!(n, 0);
+}
+#[test]
+fn n_of_streams_returns_number_of_sequential_files() {
+    clear_mocks();
+
+    let name = CString::new("foo").unwrap();
+    let name_str = name.to_str().unwrap();
+    for i in [0, 1, 2, 10] {
+        add_file(format!("{name_str}.{i}"), Vec::new());
+    }
+
+    let n = n_of_streams(name.as_ptr());
+
+    // Should only count sequential files (0,1,2) and not 10
+    assert_eq!(n, 3);
+}
 
 #[test]
 fn open_read_returns_negative_one_if_no_file() {
