f8f729236f395cfa38eba1e69a335d100afdfe8f
Oleg Parashchenko <olpa@uucode.com>
2025-02-16 05:22:46 +0100

    Extract new crate: actor_runtime_mocked.

--- ailets-rs/actor_runtime/src/lib.rs
+++ ailets-rs/actor_runtime/src/lib.rs
@@ -1,5 +1,3 @@
 mod actor_runtime;
-#[cfg(feature = "mocked")]
-pub mod mocked_actor_runtime;
 
 pub use actor_runtime::{aclose, aread, awrite, n_of_streams, open_read, open_write};
---
+++ ailets-rs/actor_runtime_mocked/src/lib.rs
@@ -0,0 +1,5 @@
+mod actor_runtime_mocked;
+
+pub use actor_runtime_mocked::{
+    clear_mocks, add_file, get_file, IO_INTERRUPT, WANT_ERROR,
+};
