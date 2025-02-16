8050ac8df84a935072410258fd477ea2f5242a89
Oleg Parashchenko <olpa@uucode.com>
2025-02-16 06:30:38 +0100

    Fix-linted "areader".

--- ailets-rs/areader/src/areader.rs
+++ ailets-rs/areader/src/areader.rs
@@ -1,48 +1,56 @@
 use std::io::{self, Read};
+use std::ffi::CStr;
 
 use actor_runtime::{aclose, aread, n_of_streams, open_read};
 
-pub struct AReader {
-    fd: Option<u32>,
-    stream_index: u32,
-    stream_name: String,
+pub struct AReader<'a> {
+    fd: Option<i32>,
+    stream_index: usize,
+    stream_name: &'a CStr,
 }
 
-impl AReader {
+impl<'a> AReader<'a> {
     #[must_use]
-    pub fn new(stream_name: &str) -> Self {
+    pub fn new(stream_name: &'a CStr) -> Self {
         AReader {
             fd: None,
             stream_index: 0,
-            stream_name: stream_name.to_string(),
+            stream_name,
         }
     }
 }
 
-impl Read for AReader {
+impl<'a> Read for AReader<'a> {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
         if self.fd.is_none() {
             let n = unsafe { n_of_streams(self.stream_name.as_ptr()) };
+            let n: usize = match n {
+                n if n < 0 => panic!("Failed to get number of streams"),
+                n => n.try_into().unwrap(),
+            };
             if self.stream_index >= n {
                 return Ok(0);
             }
             let fd = unsafe { open_read(self.stream_name.as_ptr(), self.stream_index) };
+            assert!(fd >= 0, "Failed to open read stream");
             self.fd = Some(fd);
         }
         let fd = self.fd.unwrap();
-        let bytes_read = unsafe { aread(fd, buf.as_mut_ptr(), u32::try_from(buf.len()).unwrap()) };
-        if bytes_read == 0 {
-            unsafe { aclose(fd) };
-            self.fd = None;
-
-            self.stream_index += 1;
-            return self.read(buf);
+        let bytes_read = unsafe { aread(fd, buf.as_mut_ptr(), buf.len()) };
+        match bytes_read {
+            n if n < 0 => panic!("Failed to read stream"),
+            0 => {
+                unsafe { aclose(fd) };
+                self.fd = None;
+                self.stream_index += 1;
+                self.read(buf)
+            }
+            n => Ok(usize::try_from(n).unwrap()),
         }
-        Ok(bytes_read as usize)
     }
 }
 
-impl Drop for AReader {
+impl<'a> Drop for AReader<'a> {
     fn drop(&mut self) {
         if let Some(fd) = self.fd.take() {
             unsafe {
--- ailets-rs/areader/src/lib.rs
+++ ailets-rs/areader/src/lib.rs
@@ -1,5 +1,3 @@
-pub mod mocked_actor_runtime;
 pub mod areader;
 
-pub use mocked_actor_runtime::*;
 pub use areader::AReader;
