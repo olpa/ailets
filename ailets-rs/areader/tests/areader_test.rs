a65a5430faabb9449b890e067aa19b6497333ff4
Oleg Parashchenko <olpa@uucode.com>
2025-02-18 04:50:36 +0100

    Test to check failing "read".

--- ailets-rs/areader/tests/areader_test.rs
+++ ailets-rs/areader/tests/areader_test.rs
@@ -36,11 +36,34 @@ fn cant_open_nonexistent_file() {
 fn close_can_raise_error() {
     clear_mocks();
 
-    add_file("test.0".to_string(), b"foo".to_vec());
+    add_file("fname-close-error.0".to_string(), b"foo".to_vec());
 
-    let mut reader = AReader::new(c"test").expect("Should create reader");
+    let mut reader = AReader::new(c"fname-close-error").expect("Should create reader");
 
     // Mock a close error by clearing the mocks while file is still open
     clear_mocks();
-    reader.close().expect_err("Should fail to close");
+    let err = reader.close().expect_err("Should fail to close");
+    assert!(
+        err.to_string().contains("fname-close-error"),
+        "Error message should contain the stream name"
+    );
+}
+
+#[test]
+fn read_error() {
+    clear_mocks();
+
+    add_file(
+        "fname-read-error.0".to_string(),
+        vec![actor_runtime_mocked::WANT_ERROR as u8],
+    );
+
+    let mut reader = AReader::new(c"fname-read-error").expect("Should create reader");
+    let mut buf = [0u8; 10];
+
+    let err = reader.read(&mut buf).expect_err("Should fail to read");
+    assert!(
+        err.to_string().contains("fname-read-error"),
+        "Error message should contain the stream name"
+    );
 }
