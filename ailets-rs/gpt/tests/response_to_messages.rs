2321ea27475cafa739d4ac4cca08f90d5b4b9ecb
Oleg Parashchenko <olpa@uucode.com>
2025-02-20 07:15:07 +0100

    Compile response_to_messages.

--- ailets-rs/gpt/tests/response_to_messages.rs
+++ ailets-rs/gpt/tests/response_to_messages.rs
@@ -1,5 +1,9 @@
 use gpt::_process_gpt;
 use std::io::Cursor;
+use std::io::Result;
+use std::rc::Rc;
+use std::io::Write;
+use std::borrow::BorrowMut;
 
 fn get_expected_basic_message() -> String {
     "{\"role\":\"assistant\",\"content\":[{\"type\":\"text\",\"text\":\
@@ -7,27 +11,56 @@ fn get_expected_basic_message() -> String {
         .to_string()
 }
 
+#[derive(Clone)]
+struct RcWriter {
+    inner: Rc<Vec<u8>>,
+}
+
+impl Write for RcWriter {
+    fn write(&mut self, buf: &[u8]) -> Result<usize> {
+        Rc::get_mut(&mut self.inner).unwrap().write(buf)
+    }
+
+    fn flush(&mut self) -> Result<()> {
+        Rc::get_mut(&mut self.inner).unwrap().flush()
+    }
+}
+
+impl RcWriter {
+    fn new() -> Self {
+        RcWriter {
+            inner: Rc::new(Vec::new()),
+        }
+    }
+
+    fn get_output(&self) -> String {
+        String::from_utf8_lossy(self.inner.as_slice()).to_string()
+    }
+}
+
+
 #[test]
 fn test_basic_processing() {
     let fixture_content = std::fs::read_to_string("tests/fixture/basic_response.txt")
         .expect("Failed to read fixture file 'basic_response.txt'");
     let reader = Cursor::new(fixture_content);
+    let writer = RcWriter::new();
 
-    _process_gpt(reader);
+    _process_gpt(reader, writer.clone());
 
-    let result = String::from_utf8(get_file("").unwrap()).unwrap();
+    let result = writer.get_output();
     assert_eq!(result, get_expected_basic_message());
 }
 
 #[test]
 fn test_streaming() {
-    clear_mocks();
     let fixture_content = std::fs::read_to_string("tests/fixture/basic_streaming.txt")
         .expect("Failed to read fixture file 'basic_streaming.txt'");
     let reader = Cursor::new(fixture_content);
+    let writer = RcWriter::new();
 
-    _process_gpt(reader);
+    _process_gpt(reader, writer.clone());
 
-    let result = String::from_utf8(get_file("").unwrap()).unwrap();
+    let result = writer.get_output();
     assert_eq!(result, get_expected_basic_message());
 }
