2141620b685348264fe3ef933e4b03fd84253e1e
Oleg Parashchenko <olpa@uucode.com>
2025-02-23 05:17:45 +0100

    m2m str builder: use "write_all" instead of "str".

--- ailets-rs/messages_to_markdown/src/structure_builder.rs
+++ ailets-rs/messages_to_markdown/src/structure_builder.rs
@@ -20,19 +20,14 @@ impl<W: Write> StructureBuilder<W> {
 
     pub fn start_paragraph(&mut self) {
         if self.need_para_divider {
-            self.str("\n\n");
+            self.writer.write_all(b"\n\n").unwrap();
         }
         self.need_para_divider = true;
     }
 
-    pub fn str(&mut self, text: &str) {
-        let text_bytes = text.as_bytes();
-        self.writer.write_all(text_bytes).unwrap();
-    }
-
     pub fn finish_with_newline(&mut self) {
         if self.need_para_divider {
-            self.str("\n");
+            self.writer.write_all(b"\n").unwrap();
         }
         self.need_para_divider = false;
     }
