06636fd412d47baddd6a8f1cb01ee7eb5235b279
Oleg Parashchenko <olpa@uucode.com>
2025-02-23 05:19:08 +0100

    Return "Result" from the builder.

--- ailets-rs/messages_to_markdown/src/structure_builder.rs
+++ ailets-rs/messages_to_markdown/src/structure_builder.rs
@@ -18,17 +18,19 @@ impl<W: Write> StructureBuilder<W> {
         &mut self.writer
     }
 
-    pub fn start_paragraph(&mut self) {
+    pub fn start_paragraph(&mut self) -> Result<(), std::io::Error> {
         if self.need_para_divider {
-            self.writer.write_all(b"\n\n").unwrap();
+            self.writer.write_all(b"\n\n")?;
         }
         self.need_para_divider = true;
+        Ok(())
     }
 
-    pub fn finish_with_newline(&mut self) {
+    pub fn finish_with_newline(&mut self) -> Result<(), std::io::Error> {
         if self.need_para_divider {
-            self.writer.write_all(b"\n").unwrap();
+            self.writer.write_all(b"\n")?;
         }
         self.need_para_divider = false;
+        Ok(())
     }
 }
