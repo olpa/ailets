b75a3a35eb96211b5a086dbbdc36a538b2e51784
Oleg Parashchenko <olpa@uucode.com>
2025-02-23 05:30:10 +0100

    Use "panic" in "cat".

--- ailets-rs/cat/src/lib.rs
+++ ailets-rs/cat/src/lib.rs
@@ -3,15 +3,25 @@ use std::io;
 
 #[no_mangle]
 #[allow(clippy::missing_panics_doc)]
-#[allow(clippy::expect_used)]
+#[allow(clippy::panic)]
 pub extern "C" fn execute() -> i32 {
-    let mut reader = AReader::new(c"").expect("Failed to open to read");
-    let mut writer = AWriter::new(c"").expect("Failed to open to write");
+    let mut reader = AReader::new(c"").unwrap_or_else(|e| {
+        panic!("Failed to open to read: {e}");
+    });
+    let mut writer = AWriter::new(c"").unwrap_or_else(|e| {
+        panic!("Failed to open to write: {e}");
+    });
 
-    io::copy(&mut reader, &mut writer).expect("Failed to copy");
+    io::copy(&mut reader, &mut writer).unwrap_or_else(|e| {
+        panic!("Failed to copy: {e}");
+    });
 
-    writer.close().expect("Failed to close writer");
-    reader.close().expect("Failed to close reader");
+    writer.close().unwrap_or_else(|e| {
+        panic!("Failed to close writer: {e}");
+    });
+    reader.close().unwrap_or_else(|e| {
+        panic!("Failed to close reader: {e}");
+    });
 
     0
 }
