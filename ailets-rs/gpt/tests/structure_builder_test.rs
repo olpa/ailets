89903cab6d24e536c1a2e5eef07d5b45c042eea7
Oleg Parashchenko <olpa@uucode.com>
2025-02-26 04:05:38 +0100

    Refactor structure builder tests to use the builder only.

--- ailets-rs/gpt/tests/structure_builder_test.rs
+++ ailets-rs/gpt/tests/structure_builder_test.rs
@@ -1,11 +1,7 @@
-use std::cell::RefCell;
-use std::io;
 use std::io::Write;
 
 use actor_runtime_mocked::RcWriter;
-use gpt::handlers::{on_content, on_role};
 use gpt::structure_builder::StructureBuilder;
-use scan_json::RJiter;
 
 #[test]
 fn basic_pass() {
@@ -27,47 +23,17 @@ fn basic_pass() {
 }
 
 #[test]
-fn join_multiple_content_deltas() {
-    // Arrange
-    let input = r#""role""Hello"" world""!""#;
-    let mut buffer = vec![0u8; 16];
-    let mut cursor = io::Cursor::new(input);
-    let rjiter = RJiter::new(&mut cursor, &mut buffer);
-    let writer = RcWriter::new();
-    let builder = StructureBuilder::new(writer.clone());
-    let rjiter_cell = RefCell::new(rjiter);
-    let builder_cell = RefCell::new(builder);
-
-    // Act
-    on_role(&rjiter_cell, &builder_cell);
-    on_content(&rjiter_cell, &builder_cell);
-    on_content(&rjiter_cell, &builder_cell);
-    on_content(&rjiter_cell, &builder_cell);
-    builder_cell.borrow_mut().end_message().unwrap();
-
-    // Assert
-    let expected =
-        r#"{"role":"role","content":[{"type":"text","text":"Hello world!"}]}"#.to_owned() + "\n";
-    assert_eq!(writer.get_output(), expected);
-}
-
-#[test]
 fn ignore_additional_role() {
     // Arrange
-    let input = r#""a1""a2""a3""#;
-    let mut buffer = vec![0u8; 16];
-    let mut cursor = io::Cursor::new(input);
-    let rjiter = RJiter::new(&mut cursor, &mut buffer);
     let writer = RcWriter::new();
-    let builder = StructureBuilder::new(writer.clone());
-    let rjiter_cell = RefCell::new(rjiter);
-    let builder_cell = RefCell::new(builder);
+    let mut builder = StructureBuilder::new(writer.clone());
 
     // Act
-    on_role(&rjiter_cell, &builder_cell);
-    on_role(&rjiter_cell, &builder_cell);
-    on_role(&rjiter_cell, &builder_cell);
-    builder_cell.borrow_mut().end_message().unwrap();
+    builder.begin_message();
+    builder.role("a1").unwrap();
+    builder.role("a2").unwrap(); // Should be ignored
+    builder.role("a3").unwrap(); // Should be ignored
+    builder.end_message().unwrap();
 
     // Assert
     let expected = r#"{"role":"a1","content":[]}"#.to_owned() + "\n";
@@ -77,18 +43,14 @@ fn ignore_additional_role() {
 #[test]
 fn create_message_without_input_role() {
     // Arrange
-    let input = r#""hello""#;
-    let mut buffer = vec![0u8; 16];
-    let mut cursor = io::Cursor::new(input);
-    let rjiter = RJiter::new(&mut cursor, &mut buffer);
-    let writer = RcWriter::new();
-    let builder = StructureBuilder::new(writer.clone());
-    let rjiter_cell = RefCell::new(rjiter);
-    let builder_cell = RefCell::new(builder);
+    let mut writer = RcWriter::new();
+    let mut builder = StructureBuilder::new(writer.clone());
 
-    // Act
-    on_content(&rjiter_cell, &builder_cell);
-    builder_cell.borrow_mut().end_message().unwrap();
+    // Act without "builder.role()"
+    builder.begin_message();
+    builder.begin_text_chunk().unwrap();
+    writer.write_all(b"hello").unwrap();
+    builder.end_message().unwrap();
 
     // Assert
     let expected =
@@ -99,20 +61,16 @@ fn create_message_without_input_role() {
 #[test]
 fn can_call_end_message_multiple_times() {
     // Arrange
-    let input = r#""hello""#;
-    let mut buffer = vec![0u8; 16];
-    let mut cursor = io::Cursor::new(input);
-    let rjiter = RJiter::new(&mut cursor, &mut buffer);
-    let writer = RcWriter::new();
-    let builder = StructureBuilder::new(writer.clone());
-    let rjiter_cell = RefCell::new(rjiter);
-    let builder_cell = RefCell::new(builder);
+    let mut writer = RcWriter::new();
+    let mut builder = StructureBuilder::new(writer.clone());
 
     // Act
-    on_content(&rjiter_cell, &builder_cell);
-    builder_cell.borrow_mut().end_message().unwrap();
-    builder_cell.borrow_mut().end_message().unwrap();
-    builder_cell.borrow_mut().end_message().unwrap();
+    builder.begin_message();
+    builder.begin_text_chunk().unwrap();
+    writer.write_all(b"hello").unwrap();
+    builder.end_message().unwrap();
+    builder.end_message().unwrap(); // Should be ok
+    builder.end_message().unwrap(); // Should be ok
 
     // Assert
     let expected =
