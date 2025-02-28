53a8c632aeb0e0c52483ebee166a33033d689257
Oleg Parashchenko <olpa@uucode.com>
2025-02-28 05:07:02 +0100

    Define extern functions.

--- ailets-rs/actor_runtime/src/actor_runtime.rs
+++ ailets-rs/actor_runtime/src/actor_runtime.rs
@@ -8,4 +8,11 @@ extern "C" {
     pub fn aread(fd: c_int, buffer_ptr: *mut u8, count: c_uint) -> c_int;
     pub fn awrite(fd: c_int, buffer_ptr: *const u8, count: c_uint) -> c_int;
     pub fn aclose(fd: c_int) -> c_int;
+
+    pub fn dag_value_node(value_ptr: *const u8, explain_ptr: *const c_char) -> c_int;
+    pub fn dag_alias(alias: *const c_char, node_handle: c_int) -> c_int;
+    pub fn dag_instantiate_with_deps(
+        workflow: *const c_char,
+        deps: *const u8, // later to be replaced with a map using UniFFI
+    ) -> c_int;
 }
