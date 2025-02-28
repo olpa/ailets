658438c3e67f6e25465d0e7a1a379003b0ba4d9b
Oleg Parashchenko <olpa@uucode.com>
2025-02-28 06:26:02 +0100

    DagOps: drop "dag_" prefix, change the type in parameters.

--- ailets-rs/actor_runtime/src/dagops.rs
+++ ailets-rs/actor_runtime/src/dagops.rs
@@ -3,38 +3,38 @@ use std::iter::Map;
 pub trait DagOpsTrait {
     /// # Errors
     /// From the host
-    fn dag_value_node(&self, value: &[u8], explain: &str) -> Result<u32, String>;
+    fn value_node(&self, value: &[u8], explain: &str) -> Result<u32, String>;
 
     /// # Errors
     /// From the host
-    fn dag_alias(&self, alias: &str, node_handle: u32) -> Result<u32, String>;
+    fn alias(&self, alias: &str, node_handle: u32) -> Result<u32, String>;
 
     /// # Errors
     /// From the host
-    fn dag_instantiate_with_deps(
+    fn instantiate_with_deps(
         &self,
         workflow_name: &str,
-        deps: &Map<String, String>,
+        deps: &Map<String, u32>,
     ) -> Result<u32, String>;
 }
 
 pub struct DagOps;
 
 impl DagOpsTrait for DagOps {
-    fn dag_value_node(&self, value: &[u8], explain: &str) -> Result<u32, String> {
+    fn value_node(&self, value: &[u8], explain: &str) -> Result<u32, String> {
         println!("dag_value_node: {value:?}, explain: {explain}");
         Ok(0)
     }
 
-    fn dag_alias(&self, alias: &str, node_handle: u32) -> Result<u32, String> {
+    fn alias(&self, alias: &str, node_handle: u32) -> Result<u32, String> {
         println!("dag_alias: {alias}, node_handle: {node_handle}");
         Ok(0)
     }
 
-    fn dag_instantiate_with_deps(
+    fn instantiate_with_deps(
         &self,
         workflow_name: &str,
-        deps: &Map<String, String>,
+        deps: &Map<String, u32>,
     ) -> Result<u32, String> {
         println!("dag_instantiate_with_deps: {workflow_name}, deps: {deps:?}");
         Ok(0)
