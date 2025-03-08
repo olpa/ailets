77aaad55b5d6ac4cc6129fc6853f489d7b7113f4
Oleg Parashchenko <olpa@uucode.com>
2025-03-08 04:35:52 +0100

    Add "detach_from_alias" to rust node runtime.

--- ailets-rs/actor_runtime/src/dagops.rs
+++ ailets-rs/actor_runtime/src/dagops.rs
@@ -1,4 +1,6 @@
-use crate::actor_runtime::{dag_alias, dag_instantiate_with_deps, dag_value_node};
+use crate::actor_runtime::{
+    dag_alias, dag_detach_from_alias, dag_instantiate_with_deps, dag_value_node,
+};
 use base64::engine;
 use std::io::Write;
 use std::iter::Iterator;
@@ -11,6 +13,10 @@ pub trait DagOpsTrait {
     /// # Errors
     /// From the host
     fn alias(&mut self, alias: &str, node_handle: u32) -> Result<u32, String>;
+
+    /// # Errors
+    /// From the host
+    fn detach_from_alias(&mut self, alias: &str) -> Result<(), String>;
 
     /// # Errors
     /// From the host
@@ -50,6 +56,17 @@ impl DagOpsTrait for DagOps {
 
         let handle = unsafe { dag_alias(alias.as_ptr().cast::<i8>(), node_handle) };
         u32::try_from(handle).map_err(|_| "dag_alias: error".to_string())
+    }
+
+    fn detach_from_alias(&mut self, alias: &str) -> Result<(), String> {
+        let alias = std::ffi::CString::new(alias).map_err(|e| e.to_string())?;
+
+        let result = unsafe { dag_detach_from_alias(alias.as_ptr().cast::<i8>()) };
+        if result == 0 {
+            Ok(())
+        } else {
+            Err("dag_detach_from_alias: error".to_string())
+        }
     }
 
     fn instantiate_with_deps(
