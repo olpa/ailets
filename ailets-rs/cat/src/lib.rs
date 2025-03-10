fba90466a4a582e52af14e56fdd7eabc57953e58
Oleg Parashchenko <olpa@uucode.com>
2025-03-10 04:21:25 +0100

    Optimistically extract a "err_to_c_string" function.

--- ailets-rs/cat/src/lib.rs
+++ ailets-rs/cat/src/lib.rs
@@ -2,42 +2,34 @@ use actor_io::{AReader, AWriter};
 use std::io;
 use std::os::raw::c_char;
 
+fn err_to_c_string(f: &str, e: impl std::error::Error) -> *const c_char {
+    let err = format!("Failed to {f}: {e}");
+    #[allow(clippy::unwrap_used)]
+    let err = std::ffi::CString::new(err).unwrap();
+    err.as_ptr()
+}
+
 #[no_mangle]
 #[allow(clippy::missing_panics_doc)]
-#[allow(clippy::unwrap_used)]
 pub extern "C" fn execute() -> *const c_char {
     let mut reader = match AReader::new(c"") {
         Ok(reader) => reader,
-        Err(e) => {
-            let err = format!("Failed to open to read: {e}");
-            let err = std::ffi::CString::new(err).unwrap();
-            return err.as_ptr();
-        }
+        Err(e) => return err_to_c_string("open to read", e),
     };
     let mut writer = match AWriter::new(c"") {
         Ok(writer) => writer,
-        Err(e) => {
-            let err = format!("Failed to open to write: {e}");
-            let err = std::ffi::CString::new(err).unwrap();
-            return err.as_ptr();
-        }
+        Err(e) => return err_to_c_string("open to write", e),
     };
 
     if let Err(e) = io::copy(&mut reader, &mut writer) {
-        let err = format!("Failed to copy: {e}");
-        let err = std::ffi::CString::new(err).unwrap();
-        return err.as_ptr();
+        return err_to_c_string("copy", e);
     }
 
     if let Err(e) = writer.close() {
-        let err = format!("Failed to close writer: {e}");
-        let err = std::ffi::CString::new(err).unwrap();
-        return err.as_ptr();
+        return err_to_c_string("close writer", e);
     }
     if let Err(e) = reader.close() {
-        let err = format!("Failed to close reader: {e}");
-        let err = std::ffi::CString::new(err).unwrap();
-        return err.as_ptr();
+        return err_to_c_string("close reader", e);
     }
 
     std::ptr::null()
