21ea6e93da2efccbb40fb24b208ab1165ad49f46
Oleg Parashchenko <olpa@uucode.com>
2025-03-10 04:47:15 +0100

    Move the helper to "actor_runtime".

--- ailets-rs/cat/src/lib.rs
+++ ailets-rs/cat/src/lib.rs
@@ -1,35 +1,28 @@
 use actor_io::{AReader, AWriter};
+use actor_runtime::err_to_heap_c_string;
 use std::io;
 use std::os::raw::c_char;
 
-fn err_to_c_string(f: &str, e: impl std::error::Error) -> *const c_char {
-    let err = format!("Failed to {f}: {e}");
-    #[allow(clippy::unwrap_used)]
-    let err = Box::leak(Box::new(std::ffi::CString::new(err).unwrap()));
-    err.as_ptr()
-}
-
 #[no_mangle]
-#[allow(clippy::missing_panics_doc)]
 pub extern "C" fn execute() -> *const c_char {
-    let mut reader = match AReader::new(c"no-such-file") {
+    let mut reader = match AReader::new(c"") {
         Ok(reader) => reader,
-        Err(e) => return err_to_c_string("open to read", e),
+        Err(e) => return err_to_heap_c_string(&format!("Failed to open to read: {e}")),
     };
     let mut writer = match AWriter::new(c"") {
         Ok(writer) => writer,
-        Err(e) => return err_to_c_string("open to write", e),
+        Err(e) => return err_to_heap_c_string(&format!("Failed to open to write: {e}")),
     };
 
     if let Err(e) = io::copy(&mut reader, &mut writer) {
-        return err_to_c_string("copy", e);
+        return err_to_heap_c_string(&format!("Failed to copy: {e}"));
     }
 
     if let Err(e) = writer.close() {
-        return err_to_c_string("close writer", e);
+        return err_to_heap_c_string(&format!("Failed to close writer: {e}"));
     }
     if let Err(e) = reader.close() {
-        return err_to_c_string("close reader", e);
+        return err_to_heap_c_string(&format!("Failed to close reader: {e}"));
     }
 
     std::ptr::null()
