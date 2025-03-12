f190c18ef22ca5cda286c9941eddbb4c77686898
Oleg Parashchenko <olpa@uucode.com>
2025-03-12 05:29:19 +0100

    In handlers, migrate to StructureBuilder.

--- ailets-rs/messages_to_query/src/handlers.rs
+++ ailets-rs/messages_to_query/src/handlers.rs
@@ -1,27 +1,20 @@
-use scan_json::{rjiter::jiter::Peek, RJiter, StreamOp};
+use scan_json::StreamOp;
+use crate::structure_builder::StructureBuilder;
 use std::cell::RefCell;
 use std::io::Write;
+use scan_json::RJiter;
 
-pub fn on_role<W: Write>(rjiter_cell: &RefCell<RJiter>, writer_cell: &RefCell<W>) -> StreamOp {
+pub fn on_role<W: Write>(rjiter_cell: &RefCell<RJiter>, builder_cell: &RefCell<StructureBuilder<W>>) -> StreamOp {
     let mut rjiter = rjiter_cell.borrow_mut();
-    let peeked = match rjiter.peek() {
-        Ok(p) => p,
-        Err(e) => return StreamOp::Error(Box::new(e)),
+    let role = match rjiter.next_str() {
+        Ok(r) => r,
+        Err(e) => {
+            return StreamOp::Error(
+                format!("Error getting role value. Expected string, got: {e:?}").into(),
+            )
+        }
     };
-    if peeked != Peek::String {
-        let error: Box<dyn std::error::Error> =
-            format!("Expected string for 'role' value, got {peeked:?}").into();
-        return StreamOp::Error(error);
-    }
-
-    let mut writer = writer_cell.borrow_mut();
-    if let Err(e) = writer.write_all(b"\"role\":\"") {
-        return StreamOp::Error(Box::new(e));
-    }
-    if let Err(e) = rjiter.write_long_bytes(&mut *writer) {
-        return StreamOp::Error(Box::new(e));
-    }
-    if let Err(e) = writer.write_all(b"\"\n") {
+    if let Err(e) = builder_cell.borrow_mut().add_role(role) {
         return StreamOp::Error(Box::new(e));
     }
     StreamOp::ValueIsConsumed
--- ailets-rs/messages_to_query/src/lib.rs
+++ ailets-rs/messages_to_query/src/lib.rs
@@ -1,3 +1,6 @@
+mod handlers;
+pub mod structure_builder;
+
 use actor_io::{AReader, AWriter};
 use actor_runtime::err_to_heap_c_string;
 use scan_json::RJiter;
@@ -6,48 +9,49 @@ use scan_json::{scan, BoxedAction, BoxedEndAction, ParentAndName, Trigger};
 use std::cell::RefCell;
 use std::ffi::c_char;
 use std::io::Write;
-
-mod handlers;
-pub mod structure_builder;
+use structure_builder::StructureBuilder;
 
 const BUFFER_SIZE: u32 = 1024;
 
 fn on_message_begin<W: Write>(
     _rjiter_cell: &RefCell<RJiter>,
-    writer_cell: &RefCell<W>,
+    builder_cell: &RefCell<StructureBuilder<W>>,
 ) -> StreamOp {
-    let mut writer = writer_cell.borrow_mut();
-    writer.write_all(b"{").unwrap();
+    let mut builder = builder_cell.borrow_mut();
+    builder.start_message().unwrap();
     StreamOp::None
 }
 
-fn on_message_end<W: Write>(writer_cell: &RefCell<W>) -> Result<(), Box<dyn std::error::Error>> {
-    let mut writer = writer_cell.borrow_mut();
-    writer.write_all(b"}\n").unwrap();
+fn on_message_end<W: Write>(builder_cell: &RefCell<StructureBuilder<W>>) -> Result<(), Box<dyn std::error::Error>> {
+    let mut builder = builder_cell.borrow_mut();
+    builder.end_message().unwrap();
     Ok(())
 }
 
+/// # Errors
+/// If anything goes wrong.
 pub fn _process_query<W: Write>(
     mut reader: impl std::io::Read,
     writer: W,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let mut buffer = vec![0u8; BUFFER_SIZE as usize];
     let rjiter_cell = RefCell::new(RJiter::new(&mut reader, &mut buffer));
-    let builder_cell = RefCell::new(writer);
+    let builder = StructureBuilder::new(writer);
+    let builder_cell = RefCell::new(builder);
 
     let message_begin = Trigger::new(
         Box::new(ParentAndName::new(
             "#top".to_string(),
             "#object".to_string(),
         )),
-        Box::new(on_message_begin) as BoxedAction<'_, W>,
+        Box::new(on_message_begin) as BoxedAction<'_, StructureBuilder<W>>,
     );
     let message_end = Trigger::new(
         Box::new(ParentAndName::new(
             "#top".to_string(),
             "#object".to_string(),
         )),
-        Box::new(on_message_end) as BoxedEndAction<'_, W>,
+        Box::new(on_message_end) as BoxedEndAction<'_, StructureBuilder<W>>,
     );
 
     scan(
