0c9639559bd07b57cb3ae55f39a7fe8b18f261b9
Oleg Parashchenko <olpa@uucode.com>
2025-04-08 04:56:49 +0200

    Update rs "actor_runtime".

--- ailets-rs/actor_runtime/src/actor_runtime.rs
+++ ailets-rs/actor_runtime/src/actor_runtime.rs
@@ -2,8 +2,7 @@ use std::os::raw::{c_char, c_int, c_uint};
 
 #[link(wasm_import_module = "")]
 extern "C" {
-    pub fn n_of_streams(name_ptr: *const c_char) -> c_int;
-    pub fn open_read(name_ptr: *const c_char, index: c_uint) -> c_int;
+    pub fn open_read(name_ptr: *const c_char) -> c_int;
     pub fn open_write(name_ptr: *const c_char) -> c_int;
     pub fn aread(fd: c_int, buffer_ptr: *mut u8, count: c_uint) -> c_int;
     pub fn awrite(fd: c_int, buffer_ptr: *const u8, count: c_uint) -> c_int;
