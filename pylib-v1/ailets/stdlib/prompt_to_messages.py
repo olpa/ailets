be6f4b03ff0c693f578dc9eeaace127513824d75
Oleg Parashchenko <olpa@uucode.com>
2025-04-15 04:32:41 +0200

    In prompt-to-messages, conditionally generate an error

--- pylib-v1/ailets/stdlib/prompt_to_messages.py
+++ pylib-v1/ailets/stdlib/prompt_to_messages.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 from typing import Any, Dict
 from ailets.cons.atyping import INodeRuntime, StdHandles
@@ -22,6 +23,27 @@ async def prompt_to_messages(runtime: INodeRuntime) -> None:
             keys,
         )
     )
+    # FIXME
+    if "FIXME" in json.dumps(list(messages)):
+        for i in range(3):
+            await write_all(
+                runtime,
+                StdHandles.stdout,
+                json.dumps(messages).encode("utf-8"),
+            )
+            await asyncio.sleep(0.1)
+        for i in range(3):
+            await write_all(
+                runtime,
+                StdHandles.stdout,
+                "FIXME_SHOULD_FAIL\n".encode("utf-8"),
+            )
+            await asyncio.sleep(0.1)
+        await write_all(
+            runtime,
+            StdHandles.stdout,
+            "just some text to see writing to a broken pipe\n".encode("utf-8"),
+        )
 
     await write_all(
         runtime,
