eb2fac29d81840fe173dfbb36b761276f539a4a8
Oleg Parashchenko <olpa@uucode.com>
2025-04-17 04:02:22 +0200

    Revert error-generation code which was for testing.

--- pylib-v1/ailets/stdlib/prompt_to_messages.py
+++ pylib-v1/ailets/stdlib/prompt_to_messages.py
@@ -1,4 +1,3 @@
-import asyncio
 import json
 from typing import Any, Dict
 from ailets.cons.atyping import INodeRuntime, StdHandles
@@ -23,16 +22,6 @@ async def prompt_to_messages(runtime: INodeRuntime) -> None:
             keys,
         )
     )
-    # FIXME
-    if "FIXME" in json.dumps(list(messages)):
-        for i in range(3):
-            await write_all(
-                runtime,
-                StdHandles.stdout,
-                json.dumps(messages).encode("utf-8"),
-            )
-            await asyncio.sleep(0.1)
-        raise Exception("FIXME testing: should break pipe")
 
     await write_all(
         runtime,
