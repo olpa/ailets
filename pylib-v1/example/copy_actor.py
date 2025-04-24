44e216860d3576cf789f1bba2f86e3f5e946adcb
Oleg Parashchenko <olpa@uucode.com>
2025-04-24 04:56:49 +0200

    "copy" actor.

---
+++ pylib-v1/example/copy_actor.py
@@ -0,0 +1,32 @@
+import os
+from ailets.atyping import INodeRuntime, StdHandles
+
+
+async def copy_actor(runtime: INodeRuntime) -> None:
+    buffer = bytearray(1024)
+
+    while True:
+        count = await runtime.read(StdHandles.stdin, buffer, len(buffer))
+        if count == 0:
+            break
+        if count == -1:
+            raise io_errno_to_oserror(runtime.get_errno())
+        await write_all(runtime, StdHandles.stdout, buffer[:count])
+
+
+def io_errno_to_oserror(ecode: int) -> OSError:
+    msg = "unknown error"
+    try:
+        msg = os.strerror(ecode)
+    except ValueError:
+        pass
+    return OSError(ecode, msg)
+
+
+async def write_all(runtime: INodeRuntime, fd: int, data: bytes) -> None:
+    pos = 0
+    while pos < len(data):
+        count = await runtime.write(fd, data[pos:], len(data) - pos)
+        if count == -1:
+            raise io_errno_to_oserror(runtime.get_errno())
+        pos += count
