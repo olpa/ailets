494ac361909a4bc68e4cb11b7485bedf1b5046ee
Oleg Parashchenko <olpa@uucode.com>
2025-05-12 04:28:55 +0200

    Add gemini models.

--- pylib-v1/ailets/models/well_known.py
+++ pylib-v1/ailets/models/well_known.py
@@ -5,6 +5,7 @@ ALIASES = {
     "gpt4o": "gpt-4o-mini",
     "gpt": "gpt-4.1-nano",
     "chatgpt": "chatgpt-4o-latest",
+    "gemini": "gemini-2.0-flash-lite",
 }
 
 OPENAI_GPT_DEFAULTS = {
@@ -22,14 +23,23 @@ LOCAL_DEFAULTS = {
 
 LOCAL_MODELS = ["local"]
 
+GOOGLE_DEFAULTS = {
+    "http.url": "https://generativelanguage.googleapis.com/v1beta/openai/chat/completions",
+    "ailets.model": "gpt",
+}
+
+GOOGLE_MODELS = ["gemini-2.0-flash", "gemini-2.0-flash-lite", "gemini-1.5", "gemini-2.5"]
+
 KEY_TO_DEFAULTS = {
     "gpt": OPENAI_GPT_DEFAULTS,
     "local": LOCAL_DEFAULTS,
+    "google": GOOGLE_DEFAULTS,
 }
 
 MODEL_TO_KEY = {
     **{model: "gpt" for model in OPENAI_GPT_MODELS},
     **{model: "local" for model in LOCAL_MODELS},
+    **{model: "google" for model in GOOGLE_MODELS},
 }
 
 def get_model_opts(model: str) -> Mapping[str, Any]:
