257555060dd933164b666adbb5847d0794f58ff8
Oleg Parashchenko <olpa@uucode.com>
2025-05-29 06:13:20 +0200

    Update prompt-to-messages.

--- pylib-v1/ailets/stdlib/prompt_to_messages.py
+++ pylib-v1/ailets/stdlib/prompt_to_messages.py
@@ -1,39 +1,43 @@
 import json
-from typing import Any, Dict
 from ailets.atyping import INodeRuntime, StdHandles
 from ailets.cons.util import write_all
 from ailets.io.input_reader import iter_input_objects
 
 
 async def prompt_to_messages(runtime: INodeRuntime) -> None:
-    role_to_content: Dict[str, Any] = {}
+    last_role = None
+    should_close_messages = False
+
+    async def maybe_close_messages() -> None:
+        if should_close_messages:
+            await write_all(runtime, StdHandles.stdout, b"]}\n")
+
     async for content_item in iter_input_objects(runtime, StdHandles.stdin):
-        role = "user"
-        if (
+        role = last_role
+        is_ctl_node = (
             isinstance(content_item, list)
             and len(content_item) > 0
-            and "_role" in content_item[0]
-        ):
-            role = content_item[0]["_role"]
-            del content_item[0]["_role"]
-        role_to_content.setdefault(role, []).append(content_item)
-
-    keys = list(role_to_content.keys())
-    keys.sort()
-    messages = list(
-        map(
-            lambda key: {
-                "role": key,
-                "content": role_to_content[key],
-            },
-            keys,
+            and content_item[0].get("type") == "ctl"
         )
-    )
+        if is_ctl_node:
+            role = content_item[1]["role"]  # type: ignore[index]
+        if role is None:
+            role = "user"
+        if role != last_role:
+            await maybe_close_messages()
+            should_close_messages = True
+            last_role = role
+            await write_all(
+                runtime,
+                StdHandles.stdout,
+                f'{{"role": "{last_role}", "content": [\n'.encode("utf-8"),
+            )
+        if is_ctl_node:
+            continue
 
-    for message in messages:
         await write_all(
-            runtime,
-            StdHandles.stdout,
-            json.dumps(message, sort_keys=True).encode("utf-8"),
+            runtime, StdHandles.stdout, json.dumps(content_item).encode("utf-8")
         )
-        await write_all(runtime, StdHandles.stdout, b"\n")
+        await write_all(runtime, StdHandles.stdout, b",\n")
+
+    await maybe_close_messages()
