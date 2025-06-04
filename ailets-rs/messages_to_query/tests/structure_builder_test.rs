1d7ba017777c8618fd394d22ff92193bfd104a0a
Oleg Parashchenko <olpa@uucode.com>
2025-06-04 06:03:05 +0200

    Touch the builder happy path test.

--- ailets-rs/messages_to_query/tests/structure_builder_test.rs
+++ ailets-rs/messages_to_query/tests/structure_builder_test.rs
@@ -24,8 +24,9 @@ fn create_empty_env_opts() -> EnvOpts {
 
 fn begin_message(builder: &mut StructureBuilder<RcWriter>, role: &str) {
     builder.begin_content_item().unwrap();
-    builder.add_item_type(String::from("ctl")).unwrap();
-    builder.add_role(role).unwrap();
+    builder.add_item_attribute(String::from("type"), String::from("ctl")).unwrap();
+    builder.add_item_attribute(String::from("role"), String::from(role)).unwrap();
+    builder.handle_role().unwrap();
     builder.end_content_item().unwrap();
 }
 
@@ -37,6 +38,7 @@ fn happy_path_for_text() {
 
     begin_message(&mut builder, "user");
     builder.begin_content_item().unwrap();
+    builder.add_item_attribute(String::from("type"), String::from("text")).unwrap();
     builder.begin_text().unwrap();
     write!(builder.get_writer(), "Hello!").unwrap();
     builder.end_text().unwrap();
