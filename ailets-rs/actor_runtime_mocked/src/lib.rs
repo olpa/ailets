1a13ce3a48a55c1107e0f7a4dabed10dda9b3d6a
Oleg Parashchenko <olpa@uucode.com>
2025-07-09 05:32:35 +0200

    Pass test that `actor_io` can write to a value node.

--- ailets-rs/actor_runtime_mocked/src/lib.rs
+++ ailets-rs/actor_runtime_mocked/src/lib.rs
@@ -2,4 +2,4 @@ pub mod rc_writer;
 pub mod vfs;
 
 pub use rc_writer::RcWriter;
-pub use vfs::{add_file, clear_mocks, get_file, get_value_node, IO_INTERRUPT, WANT_ERROR};
+pub use vfs::{add_file, clear_mocks, dag_value_node, get_file, get_value_node, IO_INTERRUPT, WANT_ERROR};
